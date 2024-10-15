f076fa1706be3668dcc33688c1b1ce22d43661ea
Ben Strong <bcstrong43@gmail.com>
2024-10-15 16:11:23 -0400

    Renamed Default Pawn

--- Source/LOTD/LOTDGameMode.cpp
+++ Source/LOTD/LOTDGameMode.cpp
@@ -7,7 +7,7 @@
 ALOTDGameMode::ALOTDGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/LOTDBP_Cowboy"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
